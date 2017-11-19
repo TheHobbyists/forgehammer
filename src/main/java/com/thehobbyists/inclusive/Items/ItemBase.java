03f196291ec9ed2ef296a895eaccf9dde3502f82
Austinthatsit <33360589+Austinthatsit@users.noreply.github.com>
2017-11-19 17:17:49 -0600

    added simple item creation classes and made a copper ingot item

---
+++ src/main/java/com/thehobbyists/inclusive/Items/ItemBase.java
@@ -0,0 +1,25 @@
+package com.thehobbyists.inclusive.Items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import com.thehobbyists.inclusive.main;
+
+public class ItemBase extends Item {
+    protected String name;
+
+    public ItemBase(String name) {
+        this.name = name;
+        setUnlocalizedName(name);
+        setRegistryName(name);
+    }
+
+    public void registerItemModel() {
+        main.proxy.registerItemRenderer(this, 0, name);
+    }
+
+    @Override
+    public ItemBase setCreativeTab(CreativeTabs tab) {
+        super.setCreativeTab(tab);
+        return this;
+    }
+}
--- src/main/java/com/thehobbyists/inclusive/main.java
+++ src/main/java/com/thehobbyists/inclusive/main.java
@@ -1,9 +1,16 @@
 package com.thehobbyists.inclusive;
 
+import com.thehobbyists.inclusive.Items.ModItems;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import com.thehobbyists.inclusive.Proxies.*;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = main.modId, name = main.name, version = main.version, acceptedMinecraftVersions = "[1.12.2]")
 public class main {
@@ -11,6 +18,9 @@ public class main {
     public static final String modId = "hobbyists";
     public static final String name = "Inclusive";
     public static final String version = "1.0.0";
+
+    @SidedProxy(serverSide = "com.thehobbyists.inclusive.Proxies.CommonProxy", clientSide = "com.thehobbyists.inclusive.Proxies.ClientProxy")
+    public static CommonProxy proxy;
 
     @Mod.Instance(modId)
     public static main instance;
@@ -27,6 +37,21 @@ public class main {
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber
+    public static class RegistrationHandler {
+
+        @SubscribeEvent
+        public static void registerItems(RegistryEvent.Register<Item> event) {
+            ModItems.register(event.getRegistry());
+        }
+
+        @SubscribeEvent
+        public static void registerItems(ModelRegistryEvent event) {
+            ModItems.registerModels();
+        }
 
     }
 
